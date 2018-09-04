ca091e0f51f129f6c39323ed05def23562fa25c1
zengwei <747624368@qq.com>
2018-09-04 15:22:13 +0800

    update

--- logging_demo.py
+++ logging_demo.py
@@ -1,11 +1,14 @@
 import logging
-from utils import init_logging
+import logging.config
 
-init_logging()
+__version__ = '0.0.1'
 
-logging.info('err')
+logging.config.fileConfig('logging_conf.ini', defaults={'logfilename': 'mylog.log'})
+logger = logging.getLogger('custom')
+
+logger.info('test')
 
 try:
     1/0
 except ZeroDivisionError:
-    logging.exception('')
+    logger.exception('')
