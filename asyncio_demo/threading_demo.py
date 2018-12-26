65d33023b7104a0ae15f23e4155ccc2ef4ab2026
zhhnzw <t>
2018-12-26 19:19:50 +0800

    init

---
+++ asyncio_demo/threading_demo.py
@@ -0,0 +1,28 @@
+import requests
+import time
+from threading import Thread
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36'}
+
+url = "http://www.tieba.com"
+
+
+def http_request():
+    try:
+        webPage = requests.get(url, headers=headers)
+        html = webPage.text
+        return {"context": html}
+    except Exception as e:
+        return {"error": e}
+
+
+threads = []
+t = time.time()
+for x in range(100):
+    thread = Thread(target=http_request)
+    threads.append(thread)
+    thread.start()
+
+for i in threads:
+    i.join()
+print("Thread Http Request", time.time() - t)
